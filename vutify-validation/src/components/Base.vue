7a445e8b6af7fb64a373e65419a4452e2a9ae1dc
SamuelBFavarin <samuelbfavarin@hotmail.com>
2020-02-19 23:56:14 -0300

    inicio da validacao do vuetify implementado no firebase

---
+++ vutify-validation/src/components/Base.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>input</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>contacts</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Usuários</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Access Token</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>gesture</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Signature</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>send</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Transfer</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Conecta Suite</v-toolbar-title>
+    </v-app-bar>
+
+    
+
+    <MenuMain></MenuMain>
+     
+    
+  
+  </v-app>
+</template>
+
+<script>
+
+  import MenuMain from './MenuMain';
+
+  export default {
+    props: {
+      source: String,
+    },
+    components: {
+      MenuMain,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+  }
+</script>
---
+++ vutify-validation/src/components/MenuMain.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-item-group mandatory>
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+					
+					<v-row :align="alignment" :justify="justify" style="height: 300px;">						
+						<!-- CARDS -->
+
+						<v-card class="mx-auto" width="200" height="200">
+							<v-col class="menu_icon" cols="auto">
+								<v-icon class="justify-center" x-large color="green darken-2" >contacts</v-icon>
+								<p class="justify-center">Usuários</p>
+							</v-col>
+						</v-card>								
+
+						<v-card class="mx-auto" width="200" height="200">
+							<v-col class="menu_icon" cols="auto">
+								<v-icon large color="purple darken-2">email</v-icon>
+								<p class="justify-center">Access Control</p>
+							</v-col>
+						</v-card>	
+						
+	
+						<v-card class="mx-auto" width="200" height="200">
+							<v-col class="menu_icon" cols="auto">
+								<v-icon large color="teal darken-2">gesture</v-icon>
+								<p class="justify-center">Signature</p>
+							</v-col>
+						</v-card>	
+
+	
+						<v-card class="mx-auto" width="200" height="200">
+							<v-col class="menu_icon" cols="auto">
+								<v-icon large color="orange darken-2">send</v-icon>
+								<p class="justify-center">Transfer</p>
+							</v-col>
+						</v-card>	
+					</v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-item-group>
+</template>
+
+<script>
+
+export default {
+  name: 'MenuMain',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+
+	.menu_icon {
+		text-align: center;
+		margin-top: 45px;
+	}
+
+</style>
